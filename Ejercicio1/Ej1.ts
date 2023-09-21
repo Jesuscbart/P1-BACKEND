1e054bd551f29aa558c6d4fb8d539ad06ba5f6ca
Jesuscbart <97801274+Jesuscbart@users.noreply.github.com>
2023-09-21 12:37:31 +0200

    Nuevos cambios

--- Ejercicio1/Ej1.ts
+++ Ejercicio1/Ej1.ts
@@ -1,4 +1,28 @@
 //Ejercicio 1 - Práctica 1 - Arquitectura y Programación de Sistemas en Internet
 //Jesús Cuesta Bartolomé
 
-console.log("Hola mundooooo")
+function quicksort(array: number[]): number[] {
+  if (array.length <= 1) {
+    return array;
+  }
+
+  const pivot = array[0];
+  const left = [];
+  const right = [];
+
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < pivot) {
+      left.push(array[i]);
+    } else {
+      right.push(array[i]);
+    }
+  }
+
+  return [...quicksort(left), pivot, ...quicksort(right)];
+}
+
+// Ejemplo de uso:
+const array = [5, 2, 9, 1, 5, 6];
+const sortedArray = quicksort(array);
+console.log(sortedArray); // Imprime [1, 2, 5, 5, 6, 9]
+
